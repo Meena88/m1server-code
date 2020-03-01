5fb9a23cc572a0116d2836f666c0f42bfdd5c755
Meena88 <48481481+Meena88@users.noreply.github.com>
2020-03-01 17:45:32 +0530

    Update JournalEntryData.java

--- fineract-provider/src/main/java/org/apache/fineract/accounting/journalentry/data/JournalEntryData.java
+++ fineract-provider/src/main/java/org/apache/fineract/accounting/journalentry/data/JournalEntryData.java
@@ -23,6 +23,7 @@ import java.util.List;
 
 import org.apache.fineract.accounting.glaccount.data.GLAccountData;
 import org.apache.fineract.infrastructure.bulkimport.constants.TemplatePopulateImportConstants;
+
 import org.apache.fineract.infrastructure.core.data.EnumOptionData;
 import org.apache.fineract.organisation.monetary.data.CurrencyData;
 import org.joda.time.LocalDate;
@@ -79,7 +80,93 @@ public class JournalEntryData {
 
     @SuppressWarnings("unused")
     private final TransactionDetailData transactionDetails;
+private final Long glClosureId;
 
+    //import fields
+    private transient Integer rowIndex;
+    private String dateFormat;
+    private String locale;
+    private List<CreditDebit> credits;
+    private List<CreditDebit> debits;
+    private Long paymentTypeId;
+    private String currencyCode;
+    private String accountNumber;
+    private String checkNumber;
+    private String routingCode;
+    private String receiptNumber;
+    private String bankNumber;
+
+    public static JournalEntryData importInstance(Long officeId,LocalDate transactionDate,String currencyCode, Long paymentTypeId,
+            Integer rowIndex,List<CreditDebit> credits, List<CreditDebit>debits,
+            String accountNumber,String checkNumber,String routingCode,String receiptNumber,String bankNumber,
+            String comments,String locale,String dateFormat){
+        return new JournalEntryData(officeId, transactionDate, currencyCode,
+                paymentTypeId, rowIndex, credits, debits,accountNumber,checkNumber,routingCode,
+                receiptNumber,bankNumber,comments,locale,dateFormat);
+    }
+    private JournalEntryData(Long officeId,LocalDate transactionDate,String currencyCode, Long paymentTypeId,
+            Integer rowIndex,List<CreditDebit> credits, List<CreditDebit>debits,
+            String accountNumber,String checkNumber,String routingCode,String receiptNumber,
+            String bankNumber,String comments,String locale,String dateFormat) {
+
+        this.officeId = officeId;
+        this.dateFormat= dateFormat;
+        this.locale= locale;
+        this.transactionDate = transactionDate;
+        this.currencyCode=currencyCode;
+        this.rowIndex = rowIndex;
+        this.credits = credits;
+        this.debits = debits;
+        this.paymentTypeId = paymentTypeId;
+        this.accountNumber=accountNumber;
+        this.checkNumber=checkNumber;
+        this.routingCode=routingCode;
+        this.receiptNumber=receiptNumber;
+        this.bankNumber=bankNumber;
+        this.comments=comments;
+        this.id = null;
+        this.officeName = null;
+        this.glAccountName = null;
+        this.glAccountId = null;
+        this.glAccountCode = null;
+        this.glAccountType = null;
+        this.entryType = null;
+        this.amount = null;
+        this.currency = null;
+		this.glClosureId = null;
+        this.transactionId = null;
+        this.manualEntry = null;
+        this.entityType = null;
+        this.entityId = null;
+        this.createdByUserId = null;
+        this.createdDate = null;
+        this.createdByUserName = null;
+        this.reversed = null;
+        this.referenceNumber = null;
+        this.officeRunningBalance = null;
+        this.organizationRunningBalance = null;
+        this.runningBalanceComputed = null;
+        this.transactionDetails = null;
+    }
+
+    public Integer getRowIndex() {
+        return rowIndex;
+    }
+
+    public LocalDate getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void addDebits(CreditDebit debit) {
+
+        this.debits.add(debit);
+    }
+
+
+
+    public void addCredits(CreditDebit credit) {
+        this.credits.add(credit);
+    }
     //import fields
     private transient Integer rowIndex;
     private String dateFormat;
@@ -171,7 +258,8 @@ public class JournalEntryData {
             final Boolean manualEntry, final EnumOptionData entityType, final Long entityId, final Long createdByUserId,
             final LocalDate createdDate, final String createdByUserName, final String comments, final Boolean reversed,
             final String referenceNumber, final BigDecimal officeRunningBalance, final BigDecimal organizationRunningBalance,
-            final Boolean runningBalanceComputed, final TransactionDetailData transactionDetailData, final CurrencyData currency) {
+           final Boolean runningBalanceComputed, final TransactionDetailData transactionDetailData, final CurrencyData currency,
+			final Long glClosureId) {
         this.id = id;
         this.officeId = officeId;
         this.officeName = officeName;
@@ -197,6 +285,7 @@ public class JournalEntryData {
         this.runningBalanceComputed = runningBalanceComputed;
         this.transactionDetails = transactionDetailData;
         this.currency = currency;
+        this.glClosureId = glAccountId;
     }
 
     public static JournalEntryData fromGLAccountData(final GLAccountData glAccountData) {
@@ -226,10 +315,12 @@ public class JournalEntryData {
         final Boolean runningBalanceComputed = null;
         final TransactionDetailData transactionDetailData = null;
         final CurrencyData currency = null;
+        final Long glClosureId = glAccountData.getId();
+        
         return new JournalEntryData(id, officeId, officeName, glAccountName, glAccountId, glAccountCode, glAccountClassification,
                 transactionDate, entryType, amount, transactionId, manualEntry, entityType, entityId, createdByUserId, createdDate,
                 createdByUserName, comments, reversed, referenceNumber, officeRunningBalance, organizationRunningBalance,
-                runningBalanceComputed, transactionDetailData, currency);
+                runningBalanceComputed, transactionDetailData, currency, glClosureId);
     }
 
     public Long getId() {
